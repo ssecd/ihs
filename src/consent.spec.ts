44cffdc8c113f3d46e2493bb83c1e8e338a16e4e
mustofa-id <hm.dev7@gmail.com>
2024-01-09 22:18:30 +0700

    finalize consent api

--- src/consent.spec.ts
+++ src/consent.spec.ts
@@ -14,4 +14,32 @@ describe('consent', () => {
 		expect(consent).toBeTypeOf('object');
 		expect(consent).toBeInstanceOf(Consent);
 	});
+
+	it('get method should return Consent resource', async () => {
+		const result = await consent.get(process.env.TEST_PATIENT_ID!);
+		expect(result.resourceType === 'Consent').toBe(true);
+	});
+
+	it('get method should return OperationOutcome resource', async () => {
+		const result = await consent.get('1');
+		expect(result.resourceType === 'OperationOutcome').toBe(true);
+	});
+
+	it('update method should return Consent resource', async () => {
+		const result = await consent.update({
+			patientId: process.env.TEST_PATIENT_ID!,
+			action: 'OPTIN',
+			agent: process.env.TEST_AGENT_NAME!
+		});
+		expect(result.resourceType === 'Consent').toBe(true);
+	});
+
+	it('update method should return OperationOutcome resource', async () => {
+		const result = await consent.update({
+			patientId: '',
+			action: 'OPTOUT',
+			agent: ''
+		});
+		expect(result.resourceType === 'OperationOutcome').toBe(true);
+	});
 });
--- src/consent.ts
+++ src/consent.ts
@@ -5,29 +5,94 @@ let instance: Consent | undefined;
 export class Consent {
 	constructor(private readonly ihs: IHS) {}
 
-	async get(patientId: string) {
-		const authResult = await this.ihs.auth();
-		const url = new URL(this.ihs.baseUrls.consent + '/Consent');
-		url.searchParams.set('patient_id', patientId);
-		return fetch(url, {
-			headers: {
-				Authorization: `Bearer ${authResult['access_token']}`
+	/**
+	 * Fungsi dari API ini adalah untuk mendapatkan data terkait resource Consent yang
+	 * tersedia di ekosistem SatuSehat. Jika status 2xx return `Consent` dan selain
+	 * itu return `OperationOutcome` termasuk status 5xx.
+	 *
+	 * @param patientId IHS patient id
+	 * @returns FHIR resource `Consent` or `OperationOutcome`.
+	 */
+	async get(patientId: string): Promise<fhir4.Consent | fhir4.OperationOutcome> {
+		try {
+			const authResult = await this.ihs.auth();
+			const url = new URL(this.ihs.baseUrls.consent + '/Consent');
+			url.searchParams.set('patient_id', patientId);
+			const response = await fetch(url, {
+				headers: {
+					Authorization: `Bearer ${authResult['access_token']}`
+				}
+			});
+			if (response.status >= 500) {
+				throw new Error(await response.text());
 			}
-		});
+			return await response.json();
+		} catch (error) {
+			return this.exception(error);
+		}
 	}
 
-	async update(data: { patientId: string; action: 'OPTIN' | 'OPTOUT'; agent: string }) {
-		const authResult = await this.ihs.auth();
-		const url = new URL(this.ihs.baseUrls.consent + '/Consent');
-		const payload = JSON.stringify({ patient_id: data.patientId, ...data });
-		return fetch(url, {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${authResult['access_token']}`,
-				'Content-Type': 'application/json'
-			},
-			body: payload
-		});
+	/**
+	 * Fungsi dari API ini adalah untuk melakukan perubahan data terkait resource Consent
+	 * ke dalam ekosistem SatuSehat, yang sebelumnya sudah ditambahkan dan tersedia di
+	 * dalam ekosistem SatuSehat. Jika status 2xx return `Consent` dan selain itu return
+	 * `OperationOutcome` termasuk status 5xx.
+	 *
+	 * @returns FHIR resource `Consent` or `OperationOutcome`.
+	 */
+	async update(data: {
+		/** IHS patient id yang akan dilakukan proses persetujuan */
+		patientId: string;
+
+		/**
+		 * Aksi persetujuan yang akan dilakukan. Isi dengan `OPTIN` bila akses disetujui,
+		 * sedangkan bila ditolak isi dengan `OPTOUT`. Persetujuan yang dimaksud adalah
+		 * bersedia dan menyetujui data rekam medis milik pasien diakses dari Fasilitas
+		 * Pelayanan Kesehatan lainnya melalui Platform SatuSehat untuk kepentingan
+		 * pelayanan kesehatan dan/atau rujukan. **Tidak berarti** pengiriman data rekam
+		 * medis tidak dilakukan jika pasien `OPTOUT`.
+		 */
+		action: 'OPTIN' | 'OPTOUT';
+
+		/**
+		 * Nama agen atau petugas yang ingin meminta persetujuan.
+		 */
+		agent: string;
+	}) {
+		try {
+			const authResult = await this.ihs.auth();
+			const url = new URL(this.ihs.baseUrls.consent + '/Consent');
+			const payload = JSON.stringify({ patient_id: data.patientId, ...data });
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${authResult['access_token']}`,
+					'Content-Type': 'application/json'
+				},
+				body: payload
+			});
+			if (response.status >= 500) {
+				throw new Error(await response.text());
+			}
+			return await response.json();
+		} catch (error) {
+			return this.exception(error);
+		}
+	}
+
+	private exception(error: unknown): fhir4.OperationOutcome {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const text = (error as any)?.message || 'unknown error';
+		return {
+			resourceType: 'OperationOutcome',
+			issue: [
+				{
+					code: 'exception',
+					severity: 'error',
+					details: { text }
+				}
+			]
+		};
 	}
 }
 
