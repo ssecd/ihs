31e3d6108c1cb4bdb68a5d2603d64ce63e7037a6
mustofa-id <hm.dev7@gmail.com>
2024-01-09 17:22:59 +0700

    handle auth token expiration

--- src/ihs.ts
+++ src/ihs.ts
@@ -57,6 +57,8 @@ const defaultBaseUrls: BaseURL = {
 export default class IHS {
 	readonly config: Readonly<IHSConfig>;
 
+	private authDetail: AuthDetail | undefined;
+
 	constructor(userConfig?: Partial<IHSConfig>) {
 		this.config = this.defineConfig(userConfig);
 		Object.freeze(this.config);
@@ -81,10 +83,12 @@ export default class IHS {
 		return defaultBaseUrls[this.config.mode];
 	}
 
-	async auth() {
-		const response = await this.authenticate();
-		const authResult = await response.json();
-		return authResult; // TODO: handle cache and expiration
+	async auth(): Promise<AuthDetail> {
+		if (this.authDetailExpired) {
+			const response = await this.authenticate();
+			this.authDetail = await response.json();
+		}
+		return this.authDetail!;
 	}
 
 	async fhir(
@@ -114,7 +118,7 @@ export default class IHS {
 		return instance;
 	}
 
-	private async authenticate() {
+	private async authenticate(): Promise<Response> {
 		if (!this.config.clientSecret || !this.config.secretKey) {
 			const message = `Missing credentials. The "clientSecret" and "secretKey" config are required.`;
 			throw new Error(message);
@@ -130,4 +134,43 @@ export default class IHS {
 			])
 		});
 	}
+
+	private get authDetailExpired() {
+		if (!this.authDetail) return true;
+		const issuedAt = parseInt(this.authDetail.issued_at, 10);
+		const expiresIn = parseInt(this.authDetail.expires_in, 10);
+
+		// Calculate the expiration time in milliseconds
+		const expirationTime = issuedAt + expiresIn * 1000;
+
+		// Calculate the anticipation time in milliseconds
+		const anticipation = 300; // seconds
+		const anticipationTime = anticipation * 1000;
+
+		// Calculate the time when the token is considered about to expire
+		const aboutToExpireTime = expirationTime - anticipationTime;
+
+		// Compare with the current time
+		const currentTime = Date.now();
+		return aboutToExpireTime <= currentTime;
+	}
+}
+
+export interface AuthDetail {
+	refresh_token_expires_in: string;
+	api_product_list: string;
+	api_product_list_json: string[];
+	organization_name: string;
+	'developer.email': string;
+	token_type: string;
+	/** elapsed time. eg: 1698379451736 */
+	issued_at: string;
+	client_id: string;
+	access_token: string;
+	application_name: string;
+	scope: string;
+	/** in seconds */
+	expires_in: string;
+	refresh_count: string;
+	status: string;
 }
